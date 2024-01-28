de918d392496657e6b7bdb6915319b7f73c6ba1b
Mohamed Ayman <59439627+moabdelbasset@users.noreply.github.com>
2024-01-28 19:02:24 +0000

    adding login/logout

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -3,9 +3,14 @@ from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
 
     class Meta:
         model = Profile
         fields = [
-            'id', 'owner', 'name', 'image'
+            'id', 'owner', 'name', 'image', 'is_owner'
         ]
