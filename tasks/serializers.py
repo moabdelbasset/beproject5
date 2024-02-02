4459a1ed8c9dfb01b568b722eb80e3fd6f8da7e5
Mohamed Ayman <59439627+moabdelbasset@users.noreply.github.com>
2024-02-02 20:33:18 +0000

    trying to send is_owner the FE

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -4,18 +4,15 @@ from .models import Task
 class TaskSerializer(serializers.ModelSerializer):
     # You might want to include the owner's username to display who created the task
     owner_username = serializers.ReadOnlyField(source='assigned_to.user.username')
-    is_owner = serializers.ReadOnlyField()
+    is_owner = serializers.SerializerMethodField()
     
 
     def get_is_owner(self, obj):
-        request = self.context['request']
-        return request.user == obj.owner
-    
+        request = self.context.get('request')
+        return request and obj.assigned_to.user == request.user
+
     def create(self, validated_data):
-        # Assuming you want to automatically set the 'assigned_to' field to the user who created the task
-        assigned_to = validated_data.pop('assigned_to', None)
-        task = Task.objects.create(**validated_data, assigned_to=assigned_to or self.context['request'].user.profile)
-        return task
+        return Task.objects.create(**validated_data, assigned_to=self.context['request'].user.profile)
 
     class Meta:
         model = Task
