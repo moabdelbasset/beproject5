dd0fd94e6d89f2b440ae4f116b07b4110118144e
Mohamed Ayman <59439627+moabdelbasset@users.noreply.github.com>
2024-02-02 20:06:08 +0000

    Testing the backend to be able to edit tasks

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -4,44 +4,25 @@ from .models import Task
 class TaskSerializer(serializers.ModelSerializer):
     # You might want to include the owner's username to display who created the task
     owner_username = serializers.ReadOnlyField(source='assigned_to.user.username')
-
-    class Meta:
-        model = Task
-        fields = [
-            'id', 'title', 'description', 'status', 'priority', 'due_date', 
-            'created_at', 'updated_at', 'assigned_to', 'owner_username'
-        ]
-        extra_kwargs = {
-            'assigned_to': {'read_only': True}
-        }
+    is_owner = serializers.ReadOnlyField()
+    
 
     def get_is_owner(self, obj):
         request = self.context['request']
-        return request.user == obj.owner    
-
+        return request.user == obj.owner
+    
     def create(self, validated_data):
         # Assuming you want to automatically set the 'assigned_to' field to the user who created the task
         assigned_to = validated_data.pop('assigned_to', None)
         task = Task.objects.create(**validated_data, assigned_to=assigned_to or self.context['request'].user.profile)
         return task
 
-
-
-
-
-# from rest_framework import serializers
-# from .models import Profile
-
-# class ProfileSerializer(serializers.ModelSerializer):
-#     owner = serializers.ReadOnlyField(source='owner.username')
-#     is_owner = serializers.SerializerMethodField()
-
-#     def get_is_owner(self, obj):
-#         request = self.context['request']
-#         return request.user == obj.owner
-
-#     class Meta:
-#         model = Profile
-#         fields = [
-#             'id', 'owner', 'name', 'image', 'is_owner'
-#         ]
+    class Meta:
+        model = Task
+        fields = [
+            'id', 'title', 'description', 'status', 'priority', 'due_date', 
+            'created_at', 'updated_at', 'assigned_to', 'owner_username', 'is_owner'
+        ]
+        extra_kwargs = {
+            'assigned_to': {'read_only': True}
+        }
--- tasks/views.py
+++ tasks/views.py
@@ -9,28 +9,10 @@ class TaskList(generics.ListCreateAPIView):
     serializer_class = TaskSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-    def get_queryset(self):
-        # This view should return a list of all tasks
-        # for the currently authenticated user.
-        # user_profile = self.request.user.profile
-        # return Task.objects.filter(assigned_to=user_profile)
-        return Task.objects.all()
-
     def perform_create(self, serializer):
-        # Assign the task to the current user's profile.
-        serializer.save(assigned_to=self.request.user)
+        serializer.save(owner=self.request.user)
+
 
 class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Task.objects.all()
     serializer_class = TaskSerializer
     permission_classes = [IsOwnerOrReadOnly]
-
-    # def get_serializer_context(self):
-    #     """Extra context provided to the serializer class."""
-    #     return {'request': self.request}
-
-    # def get_queryset(self):
-    #     # This view should return a task
-    #     # for the currently authenticated user.
-    #     user = self.request.user
-    #     return Task.objects.filter(assigned_to__user=user)
