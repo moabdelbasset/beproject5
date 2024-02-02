395cb39c9696e360f7b63c7828392c0026e02bc0
Mohamed Ayman <59439627+moabdelbasset@users.noreply.github.com>
2024-02-02 20:13:32 +0000

    Testing the backend to be able to edit tasks

--- tasks/views.py
+++ tasks/views.py
@@ -8,6 +8,7 @@ class TaskList(generics.ListCreateAPIView):
     """List or create tasks if logged in"""
     serializer_class = TaskSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Task.objects.all()
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
