4bf57aee665de9710bf4eb181af5ada118fc0283
Mohamed Ayman <59439627+moabdelbasset@users.noreply.github.com>
2024-02-03 04:04:05 +0000

    debuging permissions.py

--- beproject5/permissions.py
+++ beproject5/permissions.py
@@ -3,6 +3,8 @@ from rest_framework import permissions
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
+        print("Request user:", request.user)
+        print("Task assigned to:", obj.assigned_to)
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.assigned_to == request.user
