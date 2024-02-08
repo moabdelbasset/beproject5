02ea4b066a178b57129200048069a3e377e7f3bc
Mohamed Ayman <59439627+moabdelbasset@users.noreply.github.com>
2024-02-08 17:49:30 +0000

    testing permissions

--- beproject5/permissions.py
+++ beproject5/permissions.py
@@ -5,5 +5,6 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        return obj.assigned_to.owner == request.user
+        #return obj.assigned_to.owner == request.user
+        return obj.owner == request.user
         
